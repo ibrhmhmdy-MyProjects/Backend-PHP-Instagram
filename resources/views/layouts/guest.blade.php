ab4b28e752fbfc990c4e522993f17bc0f361b090
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-11-11 17:08:13 +0200

    Custom Register Page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,11 +18,13 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    {{-- <x-application-logo class="w-48 max-w-48 h-auto fill-current text-gray-600" /> --}}
+                    {{-- <img src={{asset('storage').'/instagram-logo.webp'}} alt="" class="w-60 h-60 text-gray-600"> --}}
+                    <h4 class="my-12 text-6xl text-center text-orange-600">Insta<span class="text-gray-600">Clone</span></h4>
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-0 px-6 py-2 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
